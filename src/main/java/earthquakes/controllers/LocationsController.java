2df80db7a8003baaf2f361297491bcf384c042b1
Zhanglu Wang <zhangluwang@umail.ucsb.edu>
2019-12-03 20:05:29 -0800

    zw - added the favorites column and add button

--- src/main/java/earthquakes/controllers/LocationsController.java
+++ src/main/java/earthquakes/controllers/LocationsController.java
@@ -2,7 +2,6 @@ package earthquakes.controllers;
 
 import earthquakes.osm.Place;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
@@ -11,6 +10,8 @@ import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
 import java.util.Map;
 import java.util.HashMap;
 
@@ -55,9 +56,15 @@ public class LocationsController {
 
     @GetMapping("/locations")
     public String index(Model model, OAuth2AuthenticationToken token) {
-        String uid = token.getPrincipal().getAttributes().get("id").toString();
-        Iterable<Location> locations= locationRepository.findByPlaceId(uid);
+        Iterable<Location> locations= locationRepository.findAll();
         model.addAttribute("locations", locations);
         return "locations/index";
+    }
+
+    @PostMapping("/locations/add")
+    public String add(Location location, Model model) {
+      locationRepository.save(location);
+      model.addAttribute("locations", locationRepository.findAll());
+      return "locations/index";
     }
 }
